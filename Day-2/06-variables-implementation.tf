789b44733784756b1d7105f08b974794468b36cc
sushmav18 <91127042+sushmav18@users.noreply.github.com>
2025-02-07 19:21:50 +0530

    Update 06-variables-implementation.tf

--- Day-2/06-variables-implementation.tf
+++ Day-2/06-variables-implementation.tf
@@ -53,37 +53,24 @@ resource "aws_instance" "sush-inst1" {
     subnet_id = "subnet-00d1ab063013750e4"
     key_name = "ec1-key1"
     provider = "aws.us-east-1"
+    tags = {
+    Name        = "Inst1sushma"
+    Owner       = "sushma"
+  }
 }
 
 output "public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_instance.sush-inst1.public_ip
   #here sush-inst1 is not ec2 name...its just and resource type and resource
+ # Name        = "Inst1sushma" is the ec2 name
+
 }
 
 Terminal :
-variable "aws_instance_type" {
-  description = "Input variable -- instance type"
-  type        = string
-  default     = "t2.micro"  
-}
+Outputs:
 
-provider "aws" {
-  alias = "us-east-1"
-  region = "us-east-1"
-}
+public_ip = "3.237.2.156"
 
-resource "aws_instance" "sush-inst1" {
-    ami           = "ami-04b4f1a9cf54c11d0"  # Specify an appropriate AMI ID
-    instance_type = var.aws_instance_type
-    subnet_id = "subnet-00d1ab063013750e4"
-    key_name = "ec1-key1"
-    provider = "aws.us-east-1"
-}
 
-output "public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.sush-inst1.public_ip
-  #here sush-inst1 is not ec2 name...its just and resource type and resource
-}
 ```
